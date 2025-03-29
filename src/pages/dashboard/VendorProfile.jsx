ff4954ca5ac7a38c7e6dc18cd99b893438b281fd
Naomi Ansah <ansahnaomi2014@gmail.com>
2025-03-29 22:34:59 +0000

    changes made

--- src/pages/dashboard/VendorProfile.jsx
+++ src/pages/dashboard/VendorProfile.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+
+
 const VendorProfile = () => {
-  return <div></div>;
+  return (
+ <div className="relative w-[600px] bg-white rounded-lg shadow-lg">
+     <div className="bg-gradient-to-r from green-300 to green-600 h-24 rounded-to-lg relative">
+<div className="absolute -bottom-8 left-6 w-20 h-20 bg-white rounded-full p-1 shadow-md">
+  <img src="" alt="" />
+</div>
+     </div>
+      
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ </div>
+  );
 };
 
 export default VendorProfile;
